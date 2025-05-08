52fef9294455960c22b753231cbd0b77bef7d02c
Makena <kenaebustos@gmail.com>
2025-05-08 19:02:38 -0400

    Update build track dependencies and working build

---
+++ 3DEngine.cpp
@@ -0,0 +1,42 @@
+﻿// 3DEngine.cpp : Defines the entry point for the application.
+//
+#include <fmt/core.h>
+#include "3DEngine.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+    if(!glfwInit()) {
+        fmt::print("Failed to initialize GLFW\n");
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW Window", nullptr, nullptr);
+    if (!window) {
+        fmt::print("Failed to create GLFW window\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        fmt::print("Failed to initialize GLAD\n");
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
